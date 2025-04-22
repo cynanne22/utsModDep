73c48a904cbc879b1b7534f2f82333abb3bb1b48
cynanne22 <cynthiaanne2201@gmail.com>
2025-04-22 15:09:21 +0700

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -73,9 +73,31 @@ case_B = {
     "no_of_special_requests": 0
 }
 
+case_C = {
+    "Booking_ID": "INN36276",
+    "no_of_adults": 2,
+    "no_of_children": 0,
+    "no_of_weekend_nights": 1,
+    "no_of_week_nights": 2,
+    "type_of_meal_plan": "Meal Plan 3",
+    "required_car_parking_space": 0,
+    "room_type_reserved": "Room_Type 4",
+    "lead_time": 10,
+    "arrival_year": 2017,
+    "arrival_month": 10,
+    "arrival_date": 2,
+    "market_segment_type": "Offline",
+    "repeated_guest": 0,
+    "no_of_previous_cancellations": 0,
+    "no_of_previous_bookings_not_canceled": 0,
+    "avg_price_per_room": 120.50,
+    "no_of_special_requests": 0
+}
+
 test_options = {
     "Case A": case_A,
-    "Case B": case_B
+    "Case B": case_B,
+    "Case C": case_C
 }
 
 option = st.selectbox("Test Case", list(test_options.keys()))
