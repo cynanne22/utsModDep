c903c8eef0ac49c42667a4df3bde21d5589caa39
cynanne22 <cynthiaanne2201@gmail.com>
2025-04-22 14:26:27 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,7 +53,7 @@ case_A = {
 }
 
 case_B = {
-    "Booking_ID": "INN00001",
+    "Booking_ID": "INN36275",
     "no_of_adults": 2,
     "no_of_children": 0,
     "no_of_weekend_nights": 1,
@@ -61,15 +61,15 @@ case_B = {
     "type_of_meal_plan": "Meal Plan 1",
     "required_car_parking_space": 0,
     "room_type_reserved": "Room_Type 1",
-    "lead_time": 224,
-    "arrival_year": 2017,
-    "arrival_month": 10,
-    "arrival_date": 2,
+    "lead_time": 207,
+    "arrival_year": 2018,
+    "arrival_month": 12,
+    "arrival_date": 30,
     "market_segment_type": "Offline",
     "repeated_guest": 0,
     "no_of_previous_cancellations": 0,
     "no_of_previous_bookings_not_canceled": 0,
-    "avg_price_per_room": 65,
+    "avg_price_per_room": 161.67,
     "no_of_special_requests": 0
 }
 
