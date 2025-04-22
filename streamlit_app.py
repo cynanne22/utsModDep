9d15eafd6065cf94a383aecc980efee8549ae14c
cynanne22 <cynthiaanne2201@gmail.com>
2025-04-22 14:58:25 +0700

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,29 +48,29 @@ case_A = {
     "repeated_guest": 0,
     "no_of_previous_cancellations": 0,
     "no_of_previous_bookings_not_canceled": 0,
-    "avg_price_per_room": 94.5,
+    "avg_price_per_room": 94.50,
     "no_of_special_requests": 0
 }
 
 case_B = {
-    "Booking_ID": "INN36272",
+    "Booking_ID": "INN00001",
     "no_of_adults": 2,
     "no_of_children": 0,
     "no_of_weekend_nights": 1,
-    "no_of_week_nights": 3,
+    "no_of_week_nights": 2,
     "type_of_meal_plan": "Meal Plan 1",
     "required_car_parking_space": 0,
     "room_type_reserved": "Room_Type 1",
-    "lead_time": 228,
-    "arrival_year": 2018,
+    "lead_time": 224,
+    "arrival_year": 2017,
     "arrival_month": 10,
-    "arrival_date": 17,
-    "market_segment_type": "Online",
+    "arrival_date": 2,
+    "market_segment_type": "Offline",
     "repeated_guest": 0,
     "no_of_previous_cancellations": 0,
     "no_of_previous_bookings_not_canceled": 0,
-    "avg_price_per_room": 90.95,
-    "no_of_special_requests": 2
+    "avg_price_per_room": 65.00,
+    "no_of_special_requests": 0
 }
 
 test_options = {
